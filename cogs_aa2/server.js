ae0936f10618675a9e36b75f079f0744b5b6ed68
amparo <aadavalo4@gmail.com>
2018-05-03 17:04:37 -0700

    adding ajax for conditions page

--- cogs_aa2/server.js
+++ cogs_aa2/server.js
@@ -1,12 +1,74 @@
+	/* import express library */
 	const express = require('express');
 	const app = express();
 
-	app.use(express.static('static_files/'));
-    app.use(express.static('static_files/html/'));
-
-    const sqlite3 = require('sqlite3');
+	/* import sqlite3 */
+	const sqlite3 = require('sqlite3');
 	const db = new sqlite3.Database('regions_detail.db');
 
+	/* serve the static files */
+	app.use(express.static('static_files/'));
+  app.use(express.static('static_files/html/')); /* from what i think i know we don't need this */
+
+	/* fake database for conditions */
+		const conditionsFake = {
+					'Alzheimers': {
+						title: "Alzheimer\'s Disease",
+						subtitle: "sfwefvwer",
+						description: "A progressive disease that destroys memory and other important mental functions.",
+						symptoms: "dcev",
+						treatment: "sdvedvqe",
+						comparisonImg: 'img/Parkinsons_Icon.png'
+				},
+
+					'Parkinsons': {
+						title: "Parkinson\'s Disease",
+						subtitle: "",
+						description: "A disorder of the central nervous system that affects movement, often including tremors.",
+						symptoms: "",
+						treatment: "",
+						comparisonImg: ""
+				},
+
+					'Seizure': {
+						title: "Seizure",
+						subtitle: "",
+						description: "A seizure is a sudden surge of electrical activity in the brain.",
+						symptoms: "",
+						treatment: "",
+						comparisonImg: ""
+				},
+
+					'Stroke': {
+						title: "Stroke",
+						subtitle: "",
+						description: "Damage to the brain from interruption of its blood supply.",
+						symptoms: "",
+						treatment: "",
+						comparisonImg: ""
+				},
+
+					'ALS': {
+						title: "Lou Gehrig\'s Disease",
+						subtitle: "(Amyotrophic Lateral Sclerosis - A.L.S.)",
+						description: "A nervous system disease that weakens muscles and impacts physical function.",
+						symptoms: "",
+						treatment: "",
+						comparisonImg: ""
+				},
+
+					'MS': {
+						title: "Multiple Sclerosis",
+						subtitle: "(M.S.)",
+						description: "A disease in which the immune system eats away at the protective covering of nerves.",
+						symptoms: "",
+						treatment: "",
+						comparisonImg: ""
+				},
+		}
+
+
+  /* should be able to get rid of this too */
 	app.get ('/options', (req, res) => {
 		//const allOptions = Object.keys(fakeDatabase);
 		db.all('SELECT region FROM regions_to_details', (err, rows) => {
@@ -41,6 +103,24 @@
 		);
 
 	});
+
+	/* get conditions */
+		app.get ('/conditions/:name', (req, res) => {
+			const conditionToLookup = req.params.name;
+			console.log ('conditionToLookup: ', conditionToLookup);
+
+			const val = conditionsFake[conditionToLookup];
+			console.log ('val: ', val);
+
+			if (val){
+				res.send(val);
+				//return res.redirect('/conditions_detail')
+			}
+			else {
+				res.send({});
+			}
+		});
+	/* finish conditions */
 
 	app.listen(3000, () => {
 		console.log('Server started on http://localhost:3000/');
