c49f8917ed02fc36eda410d3ab10842b069801c7
amparo <aadavalo4@gmail.com>
2018-05-04 04:07:00 -0700

    editing conditions database

--- cogs_aa2/create_database1.js
+++ cogs_aa2/create_database1.js
@@ -21,11 +21,16 @@ db1.serialize(() => {
   db1.run("CREATE TABLE url_to_image (url TEXT, title TEXT, subtitle TEXT, description TEXT, symptom TEXT, treatment TEXT, image TEXT )");
 
   // insert 4 rows of data for regions:
-  db1.run("INSERT INTO url_to_image VALUES ('Alzheimers', 'Alzheimer''s Disease', 'subtitle of Alzheimer''s Disease', 'A progressive disease that destroys memory and other important mental functions.', 'symptoms of Alzheimer', 'treatment of Alzheimer','img/Parkinsons_Icon.png')");
-  db1.run("INSERT INTO url_to_image VALUES ('Parkinsons', 'Parkinson''s Disease', 'subtitle of Parkinson''s Disease', 'A disorder of the central nervous system that affects movement, often including tremors.', 'symptoms of Parkinson', 'treatment of Parkinson','image of Parkinson')");
-  db1.run("INSERT INTO url_to_image VALUES ('Seizure','Seizure', 'subtitle of Seizure', 'A seizure is a sudden surge of electrical activity in the brain.', 'symptoms of Seizure', 'treatment of Seizure',' images of Seizure comparison')");
-  db1.run("INSERT INTO url_to_image VALUES ('Stroke','Stroke', 'subtitle of Stroke', 'Damage to the brain from interruption of its blood supply.', 'symptoms of Stroke', 'treatment of Stroke','image of Stroke')");
-  db1.run("INSERT INTO url_to_image VALUES ('ALS', 'ALS', ' Lou Gehrig''s Disease(Amyotrophic Lateral Sclerosis - A.L.S.)', 'A nervous system disease that weakens muscles and impacts physical function.', 'symptoms of ALS', 'treatment of ALS','image of ALS')");
+  db1.run("INSERT INTO url_to_image VALUES ('Alzheimers', 'Alzheimer''s Disease', '', 'A progressive disease that destroys memory and other important mental functions.', 'The symptoms of Alzheimer''s disease often come on slowly. It might start when someone has trouble recalling things that just happened or putting thoughts into words. But over time, the problems get worse. People in the later stages of the disease usually can''t live alone or care for themselves.', 'No cure exists, but medications and management strategies may temporarily improve symptoms.','img/alzheimer_brain.jpg')");
+
+  db1.run("INSERT INTO url_to_image VALUES ('Parkinsons', 'Parkinson''s Disease', '', 'A disorder of the central nervous system that affects movement, often including tremors.', 'Parkinson''s often starts with a tremor in one hand. Other symptoms are slow movement, stiffness, and loss of balance.', 'There are many medications available to treat the Parkinson''s symptoms, although none yet that reverse the effects of the disease. It is common for people with PD to take a variety of these medications — all at different doses and at different times of day — to manage symptoms.','../img/parkinson_brain.png')");
+
+  db1.run("INSERT INTO url_to_image VALUES ('Seizure','Seizure', '', 'A seizure is a sudden surge of electrical activity in the brain.', 'symptoms of Seizure', 'treatment of Seizure',' images of Seizure comparison')");
+
+  db1.run("INSERT INTO url_to_image VALUES ('Stroke','Stroke', '', 'Damage to the brain from interruption of its blood supply.', 'symptoms of Stroke', 'treatment of Stroke','image of Stroke')");
+
+  db1.run("INSERT INTO url_to_image VALUES ('ALS', 'Lou Gehrig''s Disease', '(Amyotrophic Lateral Sclerosis - A.L.S.)', 'A nervous system disease that weakens muscles and impacts physical function.', 'symptoms of ALS', 'treatment of ALS','image of ALS')");
+
   db1.run("INSERT INTO url_to_image VALUES ('MS', 'Multiple Sclerosis', 'M.S.', 'A disease in which the immune system eats away at the protective covering of nerves.', 'symptoms of M.S.', 'treatment of M.S.','image of M.S.')");
 
 
@@ -33,7 +38,7 @@ db1.serialize(() => {
 
   // print them out to confirm their contents:
   db1.each("SELECT url, title, subtitle, description, symptom, treatment, image FROM url_to_image", (err, row) => {
-      console.log(row.url + row.title + "& " + row.subtitle + ' - ' + row.symptom + row.treatment + ' - ' + row.image);
+      console.log(row.url + row.title + "hello" + row.subtitle + row.symptom + row.treatment + row.image);
   });
 });
 
--- cogs_aa2/server.js
+++ cogs_aa2/server.js
@@ -11,63 +11,6 @@
 	app.use(express.static('static_files/'));
   	app.use(express.static('static_files/html/')); /* from what i think i know we don't need this */
 
-	/* fake database for conditions */
-	/*
-		const conditionsFake = {
-					'Alzheimers': {
-						title: "Alzheimer\'s Disease",
-						subtitle: "sfwefvwer",
-						description: "A progressive disease that destroys memory and other important mental functions.",
-						symptoms: "dcev",
-						treatment: "sdvedvqe",
-						comparisonImg: 'img/Parkinsons_Icon.png'
-				},
-
-					'Parkinsons': {
-						title: "Parkinson\'s Disease",
-						subtitle: "",
-						description: "A disorder of the central nervous system that affects movement, often including tremors.",
-						symptoms: "",
-						treatment: "",
-						comparisonImg: ""
-				},
-
-					'Seizure': {
-						title: "Seizure",
-						subtitle: "",
-						description: "A seizure is a sudden surge of electrical activity in the brain.",
-						symptoms: "",
-						treatment: "",
-						comparisonImg: ""
-				},
-
-					'Stroke': {
-						title: "Stroke",
-						subtitle: "",
-						description: "Damage to the brain from interruption of its blood supply.",
-						symptoms: "",
-						treatment: "",
-						comparisonImg: ""
-				},
-
-					'ALS': {
-						title: "Lou Gehrig\'s Disease",
-						subtitle: "(Amyotrophic Lateral Sclerosis - A.L.S.)",
-						description: "A nervous system disease that weakens muscles and impacts physical function.",
-						symptoms: "",
-						treatment: "",
-						comparisonImg: ""
-				},
-
-					'MS': {
-						title: "Multiple Sclerosis",
-						subtitle: "(M.S.)",
-						description: "A disease in which the immune system eats away at the protective covering of nerves.",
-						symptoms: "",
-						treatment: "",
-						comparisonImg: ""
-				},
-		} */
 
 
   /* should be able to get rid of this too */
@@ -100,7 +43,7 @@
 		db1.all (
 			'SELECT * FROM url_to_image WHERE url=$url',
 			{
-				$url: conditionsToLookup, 
+				$url: conditionsToLookup,
 			},
 			(err, rows) => {
 				console.log(rows);
@@ -136,24 +79,6 @@
 
 	});
 
-	/* get conditions */
-	/*
-		app.get ('/conditions/:name', (req, res) => {
-			const conditionToLookup = req.params.name;
-			console.log ('conditionToLookup: ', conditionToLookup);
-
-			const val = conditionsFake[conditionToLookup];
-			console.log ('val: ', val);
-
-			if (val){
-				res.send(val);
-				//return res.redirect('/conditions_detail')
-			}
-			else {
-				res.send({});
-			}
-		}); */
-	/* finish conditions */
 
 	app.listen(3000, () => {
 		console.log('Server started on http://localhost:3000/');
